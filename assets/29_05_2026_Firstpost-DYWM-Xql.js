ff941eed74084bc28baaf8baf3cf6461570c11b2
RamaChandra53 <visitramachandra@gmail.com>
2026-05-29 21:56:22 +0530

    Updates

---
+++ assets/29_05_2026_Firstpost-DYWM-Xql.js
@@ -0,0 +1,16 @@
+var e=`---
+title: First post
+date: May 29, 2026
+description: An anonymous college communication system for students
+---
+
+
+###Hellooo 
+
+sorry, printing mistake
+
+## Hello
+alright this is good
+
+
+If you think you can't , you can , if you can, you can't - Mlon Eusk`;export{e as default};
